3b7b8bb0b667e0272cee645efc83984d6396227b
ifeanyi_ns <ifeanyi_ns@icloud.com>
2022-10-16 17:34:55 +0100

    bodyupddate

--- crypto.js
+++ crypto.js
@@ -8,3 +8,14 @@ btn.addEventListener('click', () => {
     show.style.display = 'none'
   }, 5000)
 })
+
+
+const reveal = document.getElementById('reveal')
+const butn = document.querySelector('.thirdlayer-btn');
+
+butn.addEventListener('click', () => {
+  reveal.style.display = 'block'
+  setTimeout(() => {
+    reveal.style.display = 'none'
+  }, 5000)
+})
